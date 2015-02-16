9f1dd7298d80a522dd0d6403fba3d28e19d57015
Michael Scholtz <michael.scholtz@outlook.com>
2015-02-16 17:09:12 +0100

    Converted tabs to spaces. Removed unnecessary white space.

--- classes/avatar.class.php
+++ classes/avatar.class.php
@@ -1,34 +1,34 @@
 <?php
 /*
-	Minecraft Avatar Class
+    Minecraft Avatar Class
     https://github.com/scholtzm/php-minecraft-avatars
 */
 
 require_once("base.class.php");
 
 class Avatar extends Base {
-	protected $BASE_SIZE = 8;
+    protected $BASE_SIZE = 8;
 
-	/**
-	 * Class constructor
-	 */
-	public function __construct($name, $size) {
-		parent::__construct($name, $size);
-	}
+    /**
+     * Class constructor
+     */
+    public function __construct($name, $size) {
+        parent::__construct($name, $size);
+    }
 
-	/**
-	 * Concrete implementation of the CreateImage() method.
-	 */
-	protected function createImage() {
-		$size = $this->BASE_SIZE;
+    /**
+     * Concrete implementation of the CreateImage() method.
+     */
+    protected function createImage() {
+        $size = $this->BASE_SIZE;
 
-		imagesavealpha($this->skin, true);
-		$this->image = imagecreatetruecolor($size, $size);
-		
-		// face
-		imagecopyresampled($this->image, $this->skin, 0, 0, 8, 8, $size, $size, 8, 8);
-		// face gear
-		if($this->checkHatTransparency())
-			imagecopyresampled($this->image, $this->skin, 0, 0, 40, 8, $size, $size, 8, 8);
-	}
+        imagesavealpha($this->skin, true);
+        $this->image = imagecreatetruecolor($size, $size);
+
+        // face
+        imagecopyresampled($this->image, $this->skin, 0, 0, 8, 8, $size, $size, 8, 8);
+        // face gear
+        if($this->checkHatTransparency())
+            imagecopyresampled($this->image, $this->skin, 0, 0, 40, 8, $size, $size, 8, 8);
+    }
 }
--- classes/skin.class.php
+++ classes/skin.class.php
@@ -1,53 +1,53 @@
 <?php
 /*
-	Minecraft Skin Class
-	https://github.com/scholtzm/php-minecraft-avatars
+    Minecraft Skin Class
+    https://github.com/scholtzm/php-minecraft-avatars
 */
 
 require_once("base.class.php");
 
 class Skin extends Base {
-	protected $BASE_SIZE = 32;
+    protected $BASE_SIZE = 32;
 
-	/**
-	 * Class constructor
-	 */
-	public function __construct($name, $size) {
-		parent::__construct($name, $size);
-	}
+    /**
+     * Class constructor
+     */
+    public function __construct($name, $size) {
+        parent::__construct($name, $size);
+    }
 
-	/**
-	 * Concrete implementation of the CreateImage() method.
-	 */
-	protected function createImage() {
-		imagesavealpha($this->skin, true);
+    /**
+     * Concrete implementation of the CreateImage() method.
+     */
+    protected function createImage() {
+        imagesavealpha($this->skin, true);
 
-		$this->image = $this->createTransparentImage($this->BASE_SIZE / 2, $this->BASE_SIZE);
+        $this->image = $this->createTransparentImage($this->BASE_SIZE / 2, $this->BASE_SIZE);
 
-		// face
-		imagecopyresampled($this->image, $this->skin, 4, 0, 8, 8, 8, 8, 8, 8);
-		// face gear
-		if($this->checkHatTransparency())
-			imagecopyresampled($this->image, $this->skin, 4, 0, 40, 8, 8, 8, 8, 8);
-		// body
-		imagecopyresampled($this->image, $this->skin, 4, 8, 20, 20, 8, 12, 8, 12);
-		// left arm
-		imagecopyresampled($this->image, $this->skin, 0, 8, 44, 20, 4, 12, 4, 12);
-		// right arm - must FLIP
-		imagecopyresampled($this->image, $this->skin, 12, 8, 47, 20, 4, 12, -4, 12);
-		// left leg
-		imagecopyresampled($this->image, $this->skin, 4, 20, 4, 20, 4, 12, 4, 12);
-		// right leg - must FLIP
-		imagecopyresampled($this->image, $this->skin, 8, 20, 7, 20, 4, 12, -4, 12);
-	}
+        // face
+        imagecopyresampled($this->image, $this->skin, 4, 0, 8, 8, 8, 8, 8, 8);
+        // face gear
+        if($this->checkHatTransparency())
+            imagecopyresampled($this->image, $this->skin, 4, 0, 40, 8, 8, 8, 8, 8);
+        // body
+        imagecopyresampled($this->image, $this->skin, 4, 8, 20, 20, 8, 12, 8, 12);
+        // left arm
+        imagecopyresampled($this->image, $this->skin, 0, 8, 44, 20, 4, 12, 4, 12);
+        // right arm - must FLIP
+        imagecopyresampled($this->image, $this->skin, 12, 8, 47, 20, 4, 12, -4, 12);
+        // left leg
+        imagecopyresampled($this->image, $this->skin, 4, 20, 4, 20, 4, 12, 4, 12);
+        // right leg - must FLIP
+        imagecopyresampled($this->image, $this->skin, 8, 20, 7, 20, 4, 12, -4, 12);
+    }
 
-	/**
-	 * Overriden resize method
-	 */
-	protected function resize() {
-		$imgResized = $this->CreateTransparentImage($this->size / 2, $this->size);
-		imagecopyresampled($imgResized, $this->image, 0, 0, 0, 0, $this->size / 2, $this->size, $this->BASE_SIZE / 2, $this->BASE_SIZE);
-		imagedestroy($this->image);
-		$this->image = $imgResized;
-	}
+    /**
+     * Overriden resize method
+     */
+    protected function resize() {
+        $imgResized = $this->CreateTransparentImage($this->size / 2, $this->size);
+        imagecopyresampled($imgResized, $this->image, 0, 0, 0, 0, $this->size / 2, $this->size, $this->BASE_SIZE / 2, $this->BASE_SIZE);
+        imagedestroy($this->image);
+        $this->image = $imgResized;
+    }
 }
